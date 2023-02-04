56ee2a160ade396767f28408d2a50038ab979fbe
adden <a.d.denusiv@mail.ru>
2023-02-04 16:57:52 +0300

    added gradle dependencies, added Dagger Hilt

---
+++ app/src/main/java/com/example/testtasktinkofffintech/MainApp.kt
@@ -0,0 +1,8 @@
+package com.example.testtasktinkofffintech
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApp: Application() {
+}
--- app/src/main/java/com/example/testtasktinkofffintech/presentation/MainActivity.kt
+++ app/src/main/java/com/example/testtasktinkofffintech/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.testtasktinkofffintech
+package com.example.testtasktinkofffintech.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtasktinkofffintech.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
